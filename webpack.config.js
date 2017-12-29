ecd75935a933eddd35fb1a6fe4a8aa7be8dd4ba2
zhayang <zhayang@ebay.com>
2017-12-29 15:57:13 +0800

    fix production issue

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,32 @@
+var webpack = require('webpack');
+
 module.exports = {
-  entry: [
-    './src/index.js'
-  ],
-  output: {
-    path: __dirname,
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
-      }
-    }]
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-  devServer: {
-    historyApiFallback: true,
-    contentBase: './'
-  }
+    entry : [ './src/index.js' ],
+    output : {
+        path : __dirname,
+        filename : 'bundle.js'
+    },
+    plugins : [ 
+        new webpack.DefinePlugin({
+            'process.env' : {
+                'NODE_ENV' : JSON.stringify('production')
+            }
+        }) 
+    ],
+    module : {
+        loaders : [ {
+            exclude : /node_modules/,
+            loader : 'babel',
+            query : {
+                presets : [ 'react', 'es2015', 'stage-1' ]
+            }
+        } ]
+    },
+    resolve : {
+        extensions : [ '', '.js', '.jsx' ]
+    },
+    devServer : {
+        historyApiFallback : true,
+        contentBase : './'
+    }
 };
